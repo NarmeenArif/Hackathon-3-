ca9ccacbe1dbfe06c54d2407bbb76560e904e979
NarmeenArif <syedanarmeen20@gmail.com>
2025-02-07 23:46:06 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["cdn.sanity.io"], // Allow Sanity images to load
+  },
+};
+
+export default nextConfig;
+
+
